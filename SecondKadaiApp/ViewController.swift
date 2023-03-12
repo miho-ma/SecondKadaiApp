0f7bbedd6ed98b7333a9649031f9998e6948e64f
mk4 <mihopejp@yahoo.co.jp>
2023-03-12 19:50:15 +0900

    完了

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -8,10 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue){
+    }
 
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        resultViewController.nameData = nameTextField.text!
     }
 
 
